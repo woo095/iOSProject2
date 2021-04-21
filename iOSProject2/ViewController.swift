b044800b9434b9559503b9a213c26bdd697dfdd7
woo095 <woosb095@naver.com>
2021-04-21 11:23:45 +0900

    Initial Commit

---
+++ iOSProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSProject2
+//
+//  Created by Sinchon on 2021/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
